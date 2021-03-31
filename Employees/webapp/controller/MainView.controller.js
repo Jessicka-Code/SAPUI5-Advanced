ecb1c255f8163cf055094cb0db76a03d1061e739
Jessicka-Code <jesscka099@gmail.com>
2021-03-31 07:38:34 +0000

    First Commit 02

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,15 +1,28 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.Employees.controller.MainView", {
-			onInit: function () {
+        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
 
-			}
-		});
-	});
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.byId("labelCountry").setVisible(true);
+                this.byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("Not OK");
+                this.byId("labelCountry").setVisible(false);
+                this.byId("slCountry").setVisible(false);
+            }
+        };
+
+        return Main;
+    });
